a1266aa06139f34d8422df1338f9e263ee10f41d
Gokhan Karadas <gokhan.karadas@trendyol.com>
2017-06-23 08:33:25 +0300

    Initial Commit

---
+++ Marketplace/supplier-product-management-api-client/src/main/java/com/trendyol/supplier/product/management/api/client/SupplierProductManagementApiClientApplication.java
@@ -0,0 +1,12 @@
+package com.trendyol.supplier.product.management.api.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierProductManagementApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierProductManagementApiClientApplication.class, args);
+	}
+}
---
+++ Marketplace/supplier-product-management-api-client/src/test/java/com/trendyol/supplier/product/management/api/client/SupplierProductManagementApiClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trendyol.supplier.product.management.api.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupplierProductManagementApiClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
